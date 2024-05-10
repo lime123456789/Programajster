261c3ddcdcf297e282841d74cae06fe634935b80
jepsoniada <patryk.pelczar@gmail.com>
2024-05-10 06:25:57 +0200

    essential css changes for App component

--- components/App.js
+++ components/App.js
@@ -3,21 +3,21 @@ export class App extends HTMLElement {
 	super()
 	this.attachShadow({ mode: "open" })
 	this.shadowRoot.innerHTML = `
-<div>
-  <p>Witaj Świecie</p>
-  <h1>Hello world</h1>
-  <h2>test</h2>
-  <div class="testowy">cos</div>
-  <style>
-    p {
-	background: red;
-    }
-    .testowy{
-	padding: auto;
-	border: black solid 1px;
-    }
-  </style>
-</div>
+<p>Witaj Świecie</p>
+<h1>Hello world</h1>
+<h2>test</h2>
+<div class="testowy">cos</div>
+<style>
+  :host {
+      display: block;
+      position: absolute;
+      inset: 0 0;
+  }
+  .testowy{
+      padding: auto;
+      border: black solid 1px;
+  }
+</style>
         `
     }
 }
